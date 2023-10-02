15caf05051040621c72ff286e8183c9cd945e0b6
Aleksashka-i <80205061+Aleksashka-i@users.noreply.github.com>
2023-10-02 16:52:18 +0300

    Add files via upload

--- stage_5/check.py
+++ stage_5/check.py
@@ -8,6 +8,7 @@ import torch
 import torch.nn as nn
 from train_validate import *
 from functional import *
+from prettytable import PrettyTable
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -23,6 +24,18 @@ def replace_layers(model, kernels_sz):
                         bias=False)
         i += 1
         setattr(layer, 'conv2', new)
+        
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad: continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params+=params
+    print(table, flush=True)
+    print(f"Total Trainable Params: {total_params}", flush=True)
+    return total_params
 
 if __name__ == '__main__':
     __spec__ = None
@@ -33,6 +46,7 @@ if __name__ == '__main__':
     model = resnet.__dict__["resnet32"]()
     model = model.to(device)
     model = torch.nn.DataParallel(model, device_ids=device)
+    print(count_parameters(model), flush=True)
     replace_layers(model, param['kernels_sz'])
     model_dict = model.state_dict()
     filtered_dict = {k: v for k, v in param['state_dict'].items() if k in model_dict}
@@ -40,7 +54,7 @@ if __name__ == '__main__':
     model.load_state_dict(model_dict)
     
     print(model, flush=True)
-    
+    print(count_parameters(model), flush=True)
     val_loader, _, criterion = prepare_train_validate(args, device)
 
     validate(val_loader, model, criterion, device, args)
